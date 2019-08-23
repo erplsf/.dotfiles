eb06ac98010a68d7cf50c83571f880dadb6f364c
Andriy Mykhaylyk <erp.lsf@gmail.com>
2019-08-24 00:51:53 +0200

    Some scratches

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-PACKAGES=$(cat packages.txt)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+PACKAGES=$(cat $DIR/packages.txt)
 
 if ! sudo -v; then
   echo 'Your user needs to have sudo rights!'
@@ -12,3 +14,8 @@ fi
 if pacman -Qi $PACKAGES > /dev/null; then
   echo "$PACKAGES are installed"
 fi
+
+for file in "${DIR}/configs/"* "${DIR}/configs/".[!.]* ; do
+    echo "$file -> ${HOME}/$(basename $file)"
+    ln -st "${HOME}/$(basename $file)" "$file"
+done

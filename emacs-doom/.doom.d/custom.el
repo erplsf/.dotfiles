d4fc7716be6c34f5ebb4311679808ebafb16b5b4
Andriy Mykhaylyk <erp.lsf@gmail.com>
2022-06-30 15:41:28 +0200

    updates

--- emacs-doom/.doom.d/custom.el
+++ emacs-doom/.doom.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-shift-width 2)
  '(safe-local-variable-values
    '((org-log-done . time)
      (rmsbolt-demangle . t)

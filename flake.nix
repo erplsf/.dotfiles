423fd2a6e550080c7e9a6f84103b77c3a339bb6e
Andriy Mykhaylyk <erp.lsf@gmail.com>
2022-09-27 09:35:19 +0200

    update flakes and make all follow same nixpkgs

--- flake.nix
+++ flake.nix
@@ -5,8 +5,14 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    polymc.url = "github:PolyMC/PolyMC";
-    emacs.url = "github:nix-community/emacs-overlay";
+    polymc = {
+      url = "github:PolyMC/PolyMC";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    emacs = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   description = "Config for my NixOS testing VM.";

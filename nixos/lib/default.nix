fefde0f20a800af677d862d4983ebb1380e7302f
Andriy Mykhaylyk <erp.lsf@gmail.com>
2022-11-08 09:11:06 +0100

    use or

--- nixos/lib/default.nix
+++ nixos/lib/default.nix
@@ -3,7 +3,8 @@
 rec {
   listFolders = (dir:
     lib.mapAttrs (n: v: dir + "/${n}")
-    (lib.filterAttrs (n: t: t == "directory") (builtins.readDir dir)));
+    (lib.filterAttrs (n: t: t == "directory" || t == "symlink")
+      (builtins.readDir dir)));
   systemConfigs = (dir:
     lib.mapAttrs
     (host: path: (import path { inherit inputs lib; }).systemConfig)

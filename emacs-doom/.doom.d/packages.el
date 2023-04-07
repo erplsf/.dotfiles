941ffe4e3e0c6c47f9c784222a30052857d0220d
Andriy Mykhaylyk <erp.lsf@gmail.com>
2023-04-07 12:00:41 +0200

    pin packages

--- emacs-doom/.doom.d/packages.el
+++ emacs-doom/.doom.d/packages.el
@@ -13,8 +13,15 @@
 (package! org-super-agenda)
 (package! org-edna)
 (package! doct)
-;; (unpin! transient)
-;; (unpin! forge)
+
+;; Fix for a bug: https://github.com/doomemacs/doomemacs/issues/6425 
+(package! transient
+      :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
+      :recipe (:host github :repo "magit/transient"))
+
+(package! with-editor
+          :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
+          :recipe (:host github :repo "magit/with-editor"))
 
 (unless (am/phone-p)
   (package! jsonnet-mode)

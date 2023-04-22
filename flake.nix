08807621e0354206878485ebb3228abdc686ad34
Andriy Mykhaylyk <erp.lsf@gmail.com>
2023-04-22 21:29:05 +0200

    don't make flakes follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -5,23 +5,13 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    prismlauncher = {
-      url =
-        "github:prismlauncher/prismlauncher/a5c8b166fe70c430548196f0883ca1aaff816c23";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    emacs = {
-      url = "github:nix-community/emacs-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    eks-node-viewer = {
-      url = "github:erplsf/eks-node-viewer";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    aws-sso-cli = {
-      url = "github:erplsf/aws-sso-cli";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    prismlauncher.url =
+      "github:prismlauncher/prismlauncher/a5c8b166fe70c430548196f0883ca1aaff816c23";
+    # url =
+    #   "github:prismlauncher/prismlauncher/a5c8b166fe70c430548196f0883ca1aaff816c23"; # NOTE: it worked before, now it's broken
+    emacs.url = "github:nix-community/emacs-overlay";
+    eks-node-viewer.url = "github:erplsf/eks-node-viewer";
+    aws-sso-cli.url = "github:erplsf/aws-sso-cli";
   };
 
   description = "Config for my NixOS testing VM.";

77b606e9736a0e5ece9a9612e7b550a127f1a6d3
Andriy Mykhaylyk <erp.lsf@gmail.com>
2025-08-14 00:34:32 +0200

    config
    * don't show line numbers
    * enable visual line mode globally
    * add mapping for marking transactions as pending

--- emacs-doom/.config/doom/config.el
+++ emacs-doom/.config/doom/config.el
@@ -36,7 +36,7 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type nil)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -74,3 +74,13 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(setq! global-visual-line-mode 't)
+
+;; TODO: :desc doesn't seem to work
+(when (and (modulep! :lang ledger) (modulep! :editor evil +everywhere))
+  (after! evil-ledger
+    (map! :localleader
+          :map ledger-mode-map
+          :desc "Mark current transaction as pending" "t" (cmd! (ledger-toggle-current 'pending))
+  )))
